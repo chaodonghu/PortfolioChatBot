09bd5a459d8cef3c8f51340bad61c10352e3bc6f
Dong Hu <cdhu@uwaterloo.ca>
2017-07-21 19:04:54 -0400

    Updates

--- app/components/docs/pages/intro/AboutMe.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AboutMe = () => (
-  <div className="docs-installation">
-    <p>My name is Dong and I am a software developer</p>
-  </div>
-);
-
-export default AboutMe;
--- app/components/docs/pages/intro/Intro.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Intro= () => (
-  <div className="docs-hello-world">
-    <p>INTRO PAGE</p>
-  </div>
-);
-
-export default Intro;
